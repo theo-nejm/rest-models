5056082084627c18c45e9d894af2b1f898b1d4a9
Sampaio Leal <samp14hd@gmail.com>
2021-09-03 21:50:18 -0300

    fix: removing useless filter method

--- src/Collection.ts
+++ src/Collection.ts
@@ -3,8 +3,6 @@ import { makeAutoObservable } from "mobx";
 import { api } from "./api";
 import { Model } from "./Model";
 
-type PrimaryKey = string | number;
-
 export class Collection<T> {
   private data: Model<T>[] = [];
 
@@ -50,42 +48,9 @@ export class Collection<T> {
     }
   }
 
-  async filter(filters: Array<Array<string | unknown>>, params?: AxiosRequestConfig["params"]) {
-    let fetchUrl = `${this.url}`;
-    filters.forEach((filter, index) => {
-      if (filter.length !== 2)
-        throw new Error(`Each index in you array must have a pair of filter's key/value.
-        
-Expected key/value pair, but got ${filter[0]}/${filter[1]} 
-        `)
-
-      index === 0
-      ? fetchUrl += `?${filter[0]}=${filter[1]}`
-      : fetchUrl += `&${filter[0]}=${filter[1]}`
-      
-    })
-
-    const response = await api.get<T[]>(fetchUrl, { params });
-    const newData = response.data.map(data => this.createModel(data));
-
-    this.setData(newData);
-  }
-
-  async saveAll() {
-    this.list.forEach(async model => {
-      await model.save();
-    })
-
-    return this.list;
-  }
-
   private createModel(initialData: T) {
     const model = new Model<T>({ url: this.url });
     model.setData(initialData);
     return model;
-  }
-
-  private getCachedData(primaryKey: PrimaryKey) {
-    return this.list.find((data) => data.id === primaryKey);
   }
 }
